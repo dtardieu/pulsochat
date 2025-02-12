e7b34e38dfada5e4db982e1a7ca9af8bcda613f8
dtardieu <damien.tardieu@gmail.com>
2025-02-12 11:51:08 +0100

    removed langchain and corrected osc

--- ChatHandler.py
+++ ChatHandler.py
@@ -1,6 +1,5 @@
-from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder, SystemMessagePromptTemplate
-
+import openai
+from nltk.tokenize import sent_tokenize
 
 class ChatHandler:
     """Manages the chat interactions and responses."""
@@ -15,63 +14,69 @@ class ChatHandler:
         self.meta_prompt = config["meta_prompt"]
         self.current_prompt_index = 0
         self.nb_interactions = 0
-
-        self.chat = ChatOpenAI(model=self.model_name, openai_api_key=self.api_key)
-
-        self.prompt_template = ChatPromptTemplate.from_messages(
-            [
-                SystemMessagePromptTemplate.from_template("{prompt}"),
-                MessagesPlaceholder(variable_name="messages"),
-            ]
-        )
-
-        self.chain = self.prompt_template | self.chat
+        self.client = openai.OpenAI(api_key=self.api_key)
+        self.state=0
 
     def get_initial_chatbot_value(self):
         """Returns the initial value for the chatbot (first question)."""
         return self.question_list[0]
 
     def get_current_state(self):
-        return self.current_state
+        return self.state
 
     def reset(self):
         self.current_prompt_index = 0
         self.nb_interactions = 0
 
-    def response(self, message, history=[]):
+    def response(self, message, history=[], stream=False):
         """Generates a response based on the message and conversation history."""
-        # Reset counters for a new session
-
-        if len(history)+1 <= 2:
+        if len(history) + 1 <= 2:
             self.nb_interactions = 0
             self.current_prompt_index = 0
-            self.current_state = 0
+            self.state = 0
 
-        self.current_state += 1
-        # Determine if we should use a predefined question or generate a response
-        if len(history)+1 < 2*len(self.question_list):
-            print(f"history lenght: {len(history)}")
-            print(history)
-            print(f"question: {self.question_list[(len(history)+1)//2]}")
-            response = self.question_list[(len(history)+1)//2]
+        print("1--------------------")
+        if len(history) + 1 < 2 * len(self.question_list):
+            self.state += 1
+            response = self.question_list[(len(history) + 1) // 2]
+            self.logger.log_interaction(message, response)
+            print("2--------------------")
+            yield response
         else:
-            # Update prompt index based on interactions
+            print("3--------------------")
             if self.nb_interactions >= self.prompt_list[self.current_prompt_index]["interactions"]:
+                print("4--------------------")
                 self.current_prompt_index = min(self.current_prompt_index + 1, len(self.prompt_list) - 1)
                 self.nb_interactions = 0
 
-            # Generate AI response
+            self.state += 1
             promptfull = self.meta_prompt + self.prompt_list[self.current_prompt_index]["prompt"]
+            messages = history + [{"role": "user", "content": message}]
 
-            print(history+[{"role": "user", "content": message}])
 
-            response_obj = self.chain.invoke({
-                "prompt": promptfull,
-                "messages": history+[{"role": "user", "content": message}]
-            })
-            response = response_obj.content.replace('text', 'vision').replace('Text', 'Vision')
+            print(messages)
+            response_obj = self.client.chat.completions.create(
+                model=self.model_name,
+                messages=messages,
+                temperature=0.7,
+                stream=stream
+            )
+
+            if stream:
+                generated_text, printable_text = "", ""
+                for chunk in response_obj:
+                    new_text = chunk.choices[0].delta.content or ""
+                    generated_text += new_text
+                    printable_text += new_text
+                    sentences = sent_tokenize(printable_text)
+                    if len(sentences) > 1:
+                        yield sentences[0]
+                        printable_text = new_text
+                self.logger.log_interaction(message, generated_text)
+                yield printable_text  # Return last part
+            else:
+                response = response_obj.choices[0].message.content
+                self.logger.log_interaction(message, response)
+                return response
+
             self.nb_interactions += 1
-
-        # Log and return the response
-        self.logger.log_interaction(message, response)
-        return response
